5d156a78d65452832fcce6d299c0b3f3d89b6c50
JosephUCRobotics <143100877+JosephUCRobotics@users.noreply.github.com>
2025-09-29 13:13:14 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,37 @@
+# Author: Joseph Kracht
+# Last Modified: 9/26/2025
+# Title: Math Quiz
+
 # Program #2: Math Quiz
-# Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
 
-#     247
 
-# + 129
+import random
 
-# ------
 
-# The program should allow the student to enter the answer.  
-# If the answer is correct, a message of congratulations should be displayed.  
-# If the answer is incorrect a message showing the correct answer should be displayed.  
-# The program must use a function that accomplishes part of the needed tasks.
+def display_quiz(number_1, number_2):
+    print("What is")
+    print(f'{number_1:5}')
+    print(f'+{number_2:4}')
+    print('-----')
+
+def display_answer(user_input, answer):
+    if user_input == answer:
+        print("Correct!")
+    else:
+        print("Wrong. The correct answer is",answer)
+
+# Pick random numbers
+random_number_1 = random.randint(1, 999)
+random_number_2 = random.randint(1, 999)
+
+# Get answer
+correct_answer = random_number_1 + random_number_2
+
+# Display quiz
+display_quiz(random_number_1, random_number_2)
+
+# Get input
+user_answer = int(input())
+
+# Display answer
+display_answer(user_answer, correct_answer)
